9699363eefdec60349cbd276aaa669e95269d900
pierreolivierbruneau <pierreolivierbruneau@gmail.com>
2023-11-15 16:25:52 +0100

    mymoviesfront1

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/movies', (req, res) => {
-    fetch ('https://api.themoviedb.org/3/discover/movie?api_key=f604f1de8e2cb75663d4f7bcccb3c4bd')
+    fetch ('https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OWM_API_KEY}')
     .then(response => response.json())
     .then(data => {
       res.json({data});
