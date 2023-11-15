e5a99ce8453f823cc64b03098bcb9684da60f2fd
pierreolivierbruneau <pierreolivierbruneau@gmail.com>
2023-11-15 17:37:59 +0100

    mdz

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     fetch (`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OWM_API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      res.json({data});
+      res.json({movies:data.results});
     });
  });
 
